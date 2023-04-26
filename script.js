b8a08c9365b283d0b4d68c9ae44db5bdf92a6063
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-26 13:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 function fibonacci(num) {
-	
 // your code here
-	if(num < 2){
-    return num;
+	if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
-
-  return fibonacci(num - 1) + fibonacci(num - 2);
-}	
-var num1=0;
-var num2=1;
-var sum;
-var i=0;
-for (i = 0; i < num; i++)
-{
-sum=num1+num2;
-num1=num2;
-num2=sum;
 }
-return num2;
-  }
 
-}
-fibonacci(num);
-
-module.exports = fibonacci;
+module.exports = fibonacci();
